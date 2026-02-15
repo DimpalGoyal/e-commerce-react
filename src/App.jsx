010076b318a07f07595344688ac8c6a9fcd75884
Dimpal Goyal <dimpalgoyal129@gmail.com>
2026-02-15 13:19:02 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Route, Router, Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Route></Route>
+      </Router>
+
+    </>
+  )
+}
+
+export default App
